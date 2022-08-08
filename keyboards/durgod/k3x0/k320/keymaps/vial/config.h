fd07d6f42701792fa291ca0e73eeeec501b33c40
Patrick J. Jankun <p@jankun.org>
2022-08-08 20:52:16 +0200

    reconstructing durgod k320 port

---
+++ keyboards/durgod/k3x0/k320/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID           { 0xE5, 0xEF, 0x1C, 0x0D, 0x6B, 0x2F, 0xDC, 0x1F }
+
+#define VIAL_UNLOCK_COMBO_ROWS      { 0, 3  }
+#define VIAL_UNLOCK_COMBO_COLS      { 0, 13 }
