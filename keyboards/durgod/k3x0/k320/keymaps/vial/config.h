3acb4a84094bcc087a23b8e25c18cb07ed365d96
Patrick J. Jankun <p@jankun.org>
2022-08-11 20:57:05 +0200

    durgod/k3x0: device name changed

--- keyboards/durgod/k3x0/k320/keymaps/vial/config.h
+++ keyboards/durgod/k3x0/k320/keymaps/vial/config.h
@@ -1,8 +1,15 @@
 #pragma once
 
-#define VIAL_KEYBOARD_UID           { 0xE5, 0xEF, 0x1C, 0x0D, 0x6B, 0x2F, 0xDC, 0x1F }
+#define VENDOR_ID 0xD60D
+#define MANUFACTURER Durgod
+#define PRODUCT Taurus K320 (Vial)
+#define DEVICE_VER 0x0001 // Base (no backlight)
 
-#define VIAL_UNLOCK_COMBO_ROWS      { 0, 3  }
-#define VIAL_UNLOCK_COMBO_COLS      { 0, 13 }
+#define VIAL_KEYBOARD_UID \
+    { 0xE5, 0xEF, 0x1C, 0x0D, 0x6B, 0x2F, 0xDC, 0x1F }
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 13 }
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
